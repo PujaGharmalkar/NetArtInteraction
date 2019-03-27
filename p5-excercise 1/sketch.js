846a02245ca3b299c467453c9e365f1f12024c5e
PujaGharmalkar <47188455+PujaGharmalkar@users.noreply.github.com>
2019-03-27 12:42:43 -0700

    Add files via upload

---
+++ p5-excercise 1/sketch.js
@@ -0,0 +1,17 @@
+// declare variables here
+
+
+function setup() {
+  // put setup code here --> this runs once upon launch
+
+  createCanvas(500, 500);
+  background(0);
+}
+
+
+function draw() {
+  // put drawing code here --> this loops every frame
+}
+
+
+// write custom functions here
